7ec6ad9ae52f5042a622aa43df3d4d59e23251ce
visitante <visitante@lab24.famaf.unc.edu.ar>
2024-09-11 12:36:35 -0300

    Ejercicio 1 hecho

--- Tp3/modulos/controller.sv
+++ Tp3/modulos/controller.sv
@@ -1,13 +1,21 @@
 // CONTROLLER
 
 module controller(input logic [10:0] instr,
-						output logic [3:0] AluControl,						
-						output logic reg2loc, regWrite, AluSrc, Branch,
-											memtoReg, memRead, memWrite);
+						input logic ExtIRQ,reset,ExcAck,
+						output logic [3:0] AluControl,
+						output logic reg2loc, regWrite,
+						output logic [1:0] AluSrc,
+						output logic Branch, memtoReg, memRead, memWrite,
+						output logic ERet,Exc,ExtIAck,
+						output logic [3:0] EStatus
+						);
 											
 	logic [1:0] AluOp_s;
 											
 	maindec 	decPpal 	(.Op(instr), 
+							.ExtIRQ(ExtIRQ),
+							.Reset(reset),
+							.ExcAck(ExcAck),
 							.Reg2Loc(reg2loc), 
 							.ALUSrc(AluSrc), 
 							.MemtoReg(memtoReg), 
@@ -15,8 +23,13 @@ module controller(input logic [10:0] instr,
 							.MemRead(memRead), 
 							.MemWrite(memWrite), 
 							.Branch(Branch), 
-							.ALUOp(AluOp_s));	
-					
+							.ALUOp(AluOp_s),
+							.ERet(ERet),
+							.EStatus(EStatus),
+							.Exc(Exc),
+							.ExtIAck(ExtIAck)
+							);	
+
 								
 	aludec 	decAlu 	(.funct(instr), 
 							.aluop(AluOp_s), 
--- Tp3/modulos/maindec.sv
+++ Tp3/modulos/maindec.sv
@@ -1,22 +1,21 @@
 //MAINDEC
 
 module maindec (input logic [10:0] Op,
-					input logic ExtlRQ
-					input logic reset,
+					input logic ExtIRQ,
+					input logic Reset,
 					input logic ExcAck,
 					output logic Reg2Loc,
 					output logic [1:0] ALUSrc,
 					output logic MemtoReg,RegWrite,MemRead,MemWrite,Branch,
 					output logic [1:0] ALUOp,
-					output logic ERet;
-					output logic [3:0] EStatus;
-					output logic Exc,ExtlAck
+					output logic ERet,
+					output logic [3:0] EStatus,
+					output logic Exc,ExtIAck
 					);
 					
 logic NotAnInstr;
-assign NotAnInstr = 0; //??
-always_comb
-		if (reset) begin
+always_comb begin
+		if(Reset) begin
 			Reg2Loc = 1'b0;
 			ALUSrc = 2'b0;
 			MemtoReg = 1'b0;
@@ -25,10 +24,15 @@ always_comb
 			MemWrite = 1'b0;
 			Branch = 1'b0;
 			ALUOp = 2'b0;
+			ERet = 1'b0;
 			Exc = 1'b0;
-			EStatus = 3'b0;
+			EStatus = 4'b0;
+			ExtIAck = 1'b0;
+			ExtIAck = 1'b0;
+			NotAnInstr = 1'b0;
 		end
-		else begin
+		else 
+		begin
 			casez(Op)
 				11'b11111000010 : {Reg2Loc, ALUSrc[1:0], MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp[1:0],ERet,EStatus[3:0],NotAnInstr} = 16'b0_00_0_1_0_0_0_10_0_0000_0; //LDUR
 				11'b11111000000 : {Reg2Loc, ALUSrc[1:0], MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp[1:0],ERet,EStatus[3:0],NotAnInstr} = 16'b1_01_0_0_0_1_0_00_0_0000_0; //STUR
@@ -40,17 +44,17 @@ always_comb
 				//--------------------------NUEVAS INSTRUCCIONES--------------------------------
 				11'b11010110100 : {Reg2Loc, ALUSrc[1:0], MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp[1:0],ERet,EStatus[3:0],NotAnInstr} = 16'b0_00_0_0_0_0_1_01_1_0000_0; //ERET
 				11'b11010101001 : {Reg2Loc, ALUSrc[1:0], MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp[1:0],ERet,EStatus[3:0],NotAnInstr} = 16'b1_10_0_1_0_0_0_01_0_0000_0; //MRS
-				default : {Reg2Loc, ALUSrc[1:0], MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp[1:0],ERet,EStatus[3:0],NotAnInstr} = {11'b0}{0010}{1};
+				default : {Reg2Loc, ALUSrc[1:0], MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp[1:0],ERet,EStatus[3:0],NotAnInstr} = 16'b0_00_0_0_0_0_1_01_1_0010_1;
 			endcase
 			Exc = ExtIRQ | NotAnInstr;
-		end
-		//Si ExtIRQ = 1, EStatus[3..0] debe tomar el valor “0001”
-		if(ExtIRQ) begin // Habria que hacer alguna comprobacion extra con Op?
-			11'b??????????? : {EStatus[3:0]} = 0001;
-		end
-		//ExtIAck es ‘1’ cuando ExcAck = ‘1’ y ExtIRQ = ‘1’,
-		if(ExcAck & ExtIRQ) begin
-			ExtIAck = 1;
+			//Si ExtIRQ = 1, EStatus[3..0] debe tomar el valor “0001”
+			if(ExtIRQ) begin // Habria que hacer alguna comprobacion extra con Op?
+				casez(Op)
+					11'b??????????? : {EStatus[3:0]} = 0001;
+				endcase
+			end
+			//ExtIAck es ‘1’ cuando ExcAck = ‘1’ y ExtIRQ = ‘1’,
+			ExtIAck = ExcAck & ExtIRQ;
 		end
 	end
 endmodule
