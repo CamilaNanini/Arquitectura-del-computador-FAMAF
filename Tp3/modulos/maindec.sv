3c9245fad1ed5d51de5e248eb3b9ada404c8c414
visitante <visitante@lab24.famaf.unc.edu.ar>
2024-09-11 12:45:05 -0300

    Verificar las salidas dadas por CB

--- Tp3/modulos/maindec.sv
+++ Tp3/modulos/maindec.sv
@@ -44,6 +44,7 @@ always_comb begin
 				//--------------------------NUEVAS INSTRUCCIONES--------------------------------
 				11'b11010110100 : {Reg2Loc, ALUSrc[1:0], MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp[1:0],ERet,EStatus[3:0],NotAnInstr} = 16'b0_00_0_0_0_0_1_01_1_0000_0; //ERET
 				11'b11010101001 : {Reg2Loc, ALUSrc[1:0], MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp[1:0],ERet,EStatus[3:0],NotAnInstr} = 16'b1_10_0_1_0_0_0_01_0_0000_0; //MRS
+				//11'b11010110000 : {Reg2Loc, ALUSrc[1:0], MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp[1:0],ERet,EStatus[3:0],NotAnInstr} = 16'b0_00_0_0_0_0_0_10_0_0000_0; //CB  --> Revisar
 				default : {Reg2Loc, ALUSrc[1:0], MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp[1:0],ERet,EStatus[3:0],NotAnInstr} = 16'b0_00_0_0_0_0_1_01_1_0010_1;
 			endcase
 			Exc = ExtIRQ | NotAnInstr;
